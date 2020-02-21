a4b1bf9d2a0e9f0340c188e99869c21ca6c369eb
Supbads <snutz@mail.bg>
2020-02-21 02:30:58 +0200

    Initial commit

---
+++ TuringStrings/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Solution 
+{
+    public static int MinimumConcat(string initial, string goal)
+    {
+        bool isGoalCosntructable = IsGoalConstructable(initial, goal);
+
+        if (!isGoalCosntructable)
+        {
+            return -1;
+        }
+
+        int initialSize = initial.Length;
+        int goalLength = goal.Length;
+        int result = 0;
+
+        for (int goalOffset = 0; goalOffset < goalLength;)
+        {
+            if(goalOffset + initialSize > goalLength)
+            {
+                initialSize = goalLength - (goalOffset);
+            }
+
+            var goalSubstring = goal.Substring(goalOffset, initialSize);
+            var maximumGoalCoverage = GetMaximumCoverage(initial.ToCharArray(), goalSubstring.ToCharArray());
+            goalOffset += maximumGoalCoverage;
+            result++;
+        }
+
+        //check if all characters are present for both strings
+        // take N = initial.Count chars from goal
+        // check them , if they work skip next N otherwise check N - 1
+        // offset position in goal
+        // repeat step 2, 3, 4
+        
+        return result;
+    }
+    
+    private static int GetMaximumCoverage(char[] initial, char[] goalSubstring)
+    {
+        int result = 0;
+        int initialOffset = 0;
+        
+        for (int i = 0; i < goalSubstring.Length; i++)
+        {
+            bool foundCharacter = false;
+
+            var goalChar = goalSubstring[i];
+
+            for (int j = initialOffset; j < initial.Length; j++)
+            {
+                initialOffset = j;
+                if (initial[j] == goalChar)
+                {
+                    foundCharacter = true;
+                    
+                    break;
+                }
+            }
+            if (foundCharacter)
+            {
+                result++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    private static bool IsGoalConstructable(string initial, string goal)
+    {
+        HashSet<char> initialChars = new HashSet<char>();
+
+        foreach (var initialChar in initial.ToCharArray())
+        {
+            initialChars.Add(initialChar);
+        }
+
+        foreach (char goalChar in goal.ToCharArray())
+        {
+            if (!initialChars.Contains(goalChar))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var initial = Console.ReadLine();
+            var goal = Console.ReadLine();
+            var minimumConcat = Solution.MinimumConcat(initial, goal);
+            Console.WriteLine(minimumConcat);
+        }
+    }
+}
---
+++ TuringTest/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+public class Office
+{
+    private const char Floor = '1';
+    private const char Wall = '0';
+
+    private readonly char[][] _officeGrid;
+    private readonly int _rowsCount;
+    private readonly int _columnsCount;
+    private bool[][] _visitedTiles;
+
+    public Office(char[][] officeGrid)
+    {
+        this._officeGrid = officeGrid;
+        this._rowsCount = officeGrid.GetLength(0);
+        this._columnsCount = _officeGrid[0].GetLength(0);
+        this.InitVisitedFloors();
+    }
+
+    public int GetNumberOfOffices()
+    {
+        int result = 0;
+
+        for (int row = 0; row < _rowsCount; row++)
+        {
+            for (int col = 0; col < _columnsCount; col++)
+            {
+                bool isOffice = IsOffice(row, col);
+
+                if (isOffice)
+                {
+                    result++;
+                }
+            }
+        }
+        
+
+        return result;
+    }
+
+    public bool IsOffice(int row, int col)
+    {
+        if(!CanVisitTile(row, col))
+        {
+            return false;
+        }
+
+        bool isOffice = IsOfficeTile(row, col);
+        if (isOffice)
+        {
+            VisitAdjacentFloors(row, col);
+        }
+
+        return isOffice;
+    }
+
+    public void VisitAdjacentFloors(int row, int col)
+    {
+        var isRightTileOffice = IsOfficeTile(row, col + 1);
+        if (isRightTileOffice)
+        {
+            VisitAdjacentFloors(row, col + 1);
+        }
+
+        var isLeftTileOffice = IsOfficeTile(row + 1, col);
+        if (isLeftTileOffice)
+        {
+            VisitAdjacentFloors(row + 1, col);
+        }
+    }
+
+    private bool IsOfficeTile(int row, int col)
+    {
+        bool canVisitTile = CanVisitTile(row, col);
+        if (!canVisitTile)
+        {
+            return false;
+        }
+
+        this._visitedTiles[row][col] = true;
+        var tile = this._officeGrid[row][col];
+
+        return tile == '1';
+    }
+
+    private bool CanVisitTile(int row, int col)
+    {
+        if (row < 0 || row >= this._rowsCount)
+        {
+            return false;
+        }
+
+        if (col < 0 || col >= this._columnsCount)
+        {
+            return false;
+        }
+
+        if (_visitedTiles[row][col])
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void InitVisitedFloors()
+    {
+        this._visitedTiles = new bool[this._rowsCount][];
+        for (int i = 0; i < this._rowsCount; i++)
+        {
+            this._visitedTiles[i] = new bool[this._columnsCount];
+        }
+    }
+}
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var matrix = GetMatrix();
+            var office = new Office(matrix);
+            var numOffices = office.GetNumberOfOffices();
+            Console.WriteLine(numOffices);
+        }
+
+        public static char[][] GetMatrix()
+        {
+            var rows = int.Parse(Console.ReadLine());
+            var cols = int.Parse(Console.ReadLine());
+
+            char[][] matrix = new char[rows][];
+            for (var i = 0; i < rows; i++)
+            {
+                var line = Console.ReadLine();
+                matrix[i] = line.ToCharArray();
+            }
+            return matrix;
+        }
+    }
+}
